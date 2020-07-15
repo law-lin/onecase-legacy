b0a893be0620692d74d7cf6c4cf20ee99ebd464a
law-lin <lawrencelin101@gmail.com>
2020-07-15 02:59:11 -0400

    firebase deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import EarlyAccessLandingPage from "./components/EarlyAccessLandingPage";
 import LandingPage from "./components/LandingPage";
@@ -17,14 +17,12 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      // <ProfilePage/>
       <Router>
         <Route exact path={ROUTES.LANDING} component={EarlyAccessLandingPage} />
         <Route exact path={"/welcome"} component={WelcomePage} />
         <Route exact path={"/:username"} component={ProfilePage} />
         <Route exact path={"/:username/:cardTitle"} component={Bridge} />
         <Route path={ROUTES.ADMIN} component={AdminPage} />
-        {/* <Route path={ROUTES.CARD_BRIDGE} /> */}
       </Router>
     );
   }
