18650262e9d91e5d1634ae5e1d5696f56510cb80
law-lin <lawrencelin101@gmail.com>
2020-07-26 00:22:13 -0400

    landing page merge

--- src/App.js
+++ src/App.js
@@ -3,22 +3,32 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import EarlyAccessLandingPage from "./components/EarlyAccessLandingPage";
 import WelcomePage from "./components/WelcomePage";
+import LandingPage from "./components/LandingPage";
+import ProfilePage from "./components/Profile/ProfilePage";
+import AdminPage from "./components/AdminPage";
+import FeedPage from "./components/FeedPage";
 
+import Bridge from "./components/Profile/Bridge";
 import * as ROUTES from "./constants/routes";
 
 import { withAuthentication } from "./components/Session";
 
+// import AuthRoute from './components/AuthRoute'
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route exact path={ROUTES.LANDING} component={EarlyAccessLandingPage} />
+        <Route exact path={ROUTES.LANDING} component={LandingPage} />
+        {/* <Route exact path={ROUTES.LANDING} component={EarlyAccessLandingPage} /> */}
         <Route exact path={ROUTES.WELCOME} component={WelcomePage} />
+        <Route exact path={ROUTES.FEED} component={FeedPage} />
+        <Route exact path={ROUTES.USERNAME} component={ProfilePage} />
+        <Route exact path={ROUTES.USERNAME_CARD} component={Bridge} />
+        <Route path={ROUTES.ADMIN} component={AdminPage} />
       </Router>
     );
   }
 }
-
 export default withAuthentication(App);
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from "react";
+
+import { ReactComponent as TwitterIcon } from "./icons/twitter.svg";
+import { ReactComponent as InstagramIcon } from "./icons/instagram.svg";
+import { ReactComponent as YouTubeIcon } from "./icons/youtube.svg";
+
 import "./landingpage.css";
 
 import LandingPageNavbar from "./LandingPageNavbar";
-import background from "../images/background3.png";
+import background from "../images/background2.png";
+import platform from "../images/user-centered-platform.png";
+import potential from "../images/untapped-potential.png";
+import hub from "../images/creative-and-pro-hub.png";
+import lightbulb from "../images/lightbulb.png";
+import scrapbook from "../images/scrapbook.png";
+import community from "../images/community.png";
+import roundedlogo from "../images/roundedlogo.png";
 
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
@@ -305,24 +317,14 @@ function LandingPage(props) {
               marginTop: "8vh",
             }}
           >
-            <img
-              className="lightbulb"
-              src="https://www.amt-us.com/wp-content/uploads/2018/10/icon-lightbulb-2.png"
-              alt="lightbulb icon"
-            />
             <p className="description">
               We're an
               <span className="one-liner"> online scrapbook </span>
               or portfolio, with
               <span className="one-liner"> friends</span>
             </p>
-            <img
-              className="scrapbook"
-              src="https://icons.iconarchive.com/icons/flameia/machemicals/128/scrapbook-icon.png"
-              alt="scrapbook icon"
-            />
           </Grid>
-          <Grid item xs={12} style={{ margin: "0 4.5% 0 4.5%" }}>
+          <Grid item xs={12} style={{ margin: "0 10% 0 10%" }}>
             <Grid
               container
               justify="center"
@@ -330,25 +332,168 @@ function LandingPage(props) {
               spacing={3}
             >
               <Grid item xs={12} sm={4} align="center">
-                <div className="first">Your Own Page</div>
-                <div className="first-description">
+                <img
+                  className="scrapbook"
+                  src={scrapbook}
+                  alt="scrapbook icon"
+                />
+                <div className="three-headers">Your Own Page</div>
+                <div className="three-descriptions">
                   Let OneCase serve as your one-stop shop to display all your
                   favorite projects and things
                 </div>
               </Grid>
               <Grid item xs={12} sm={4} align="center">
-                <div className="second">Interest Oriented</div>
-                <div className="second-description">
+                <img
+                  className="lightbulb"
+                  src={lightbulb}
+                  alt="lightbulb icon"
+                />
+                <div className="three-headers">Interest Oriented</div>
+                <div className="three-descriptions">
                   Uploading content shouldn’t feel too personal and daunting,
                   let your interests speak for themselves
                 </div>
               </Grid>
               <Grid item xs={12} sm={4} align="center">
-                <div className="third">Creative Motivation</div>
-                <div className="third-description">
+                <img
+                  className="community"
+                  src={community}
+                  alt="community icon"
+                />
+                <div className="three-headers">Creative Motivation</div>
+                <div className="three-descriptions">
                   Get inspo from your friends, collaborate, and get excited to
                   try/learn new things
                 </div>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          spacing={5}
+          className="three-icons"
+          style={{ marginTop: "15vh" }}
+        >
+          <Grid item xs={12} style={{ margin: "0 5% 0 5%" }}>
+            <Grid container alignItems="center" spacing={5}>
+              <Grid item xs={12} sm={6} align="center">
+                <img
+                  style={{ maxWidth: "70%" }}
+                  src={platform}
+                  alt="User Centered Platform"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} align="center">
+                <div className="three-headers">User Centered Platform</div>
+                <div className="three-image-descriptions">
+                  We’re a social network that revolves around people’s geniune
+                  interests and personalities
+                </div>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} style={{ margin: "0 5% 0 5%" }}>
+            <Grid container alignItems="center" spacing={5}>
+              <Grid item xs={12} sm={6} align="center">
+                <div className="three-headers">Untapped Potential</div>
+                <div className="three-image-descriptions">
+                  A lot of the most interesting things we do go unbroadcasted or
+                  unrecorded, this is a place to share it with others
+                </div>
+              </Grid>
+              <Grid item xs={12} sm={6} align="center">
+                <img
+                  style={{ maxWidth: "70%" }}
+                  src={potential}
+                  alt="Untapped Potential"
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} style={{ margin: "0 5% 0 5%" }}>
+            <Grid container alignItems="center" spacing={5}>
+              <Grid item xs={12} sm={6} align="center">
+                <img
+                  style={{ maxWidth: "70%" }}
+                  src={hub}
+                  alt="Creative and Professional Hub"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} align="center">
+                <div className="three-headers">
+                  Creative and Professional Hub
+                </div>
+                <div className="three-image-descriptions">
+                  OneCase gives you a platform to showcase your work and
+                  interests, as well as connect with others
+                </div>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid container className="footer">
+          <Grid item xs={12}>
+            <Grid container>
+              <Grid item xs={12} style={{ margin: "0 7% 0 7%" }}>
+                <Grid container>
+                  <Grid
+                    item
+                    xs={12}
+                    sm={2}
+                    align="center"
+                    style={{ marginTop: "2%" }}
+                  >
+                    <img src={roundedlogo} />
+                    <span className="footer-logo"> OneCase</span>
+                  </Grid>
+                  <Grid item xs={12} sm={10}></Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={12} style={{ margin: "2.5% 7% 2% 7%" }}>
+                <Grid container alignItems="center">
+                  <Grid container item xs={12} sm={8} align="center">
+                    <Grid item xs={12} sm={3} style={{ marginTop: "10px" }}>
+                      <span className="footer-links">About Us</span>
+                    </Grid>
+                    <Grid item xs={12} sm={3} style={{ marginTop: "10px" }}>
+                      <span className="footer-links">Terms of Service</span>
+                    </Grid>
+                    <Grid item xs={12} sm={3} style={{ marginTop: "10px" }}>
+                      <span className="footer-links">Privacy Policy </span>
+                    </Grid>
+                    <Grid item xs={12} sm={3} style={{ marginTop: "10px" }}>
+                      <span className="footer-links">Contact</span>
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={12} sm={2} />
+                  <Grid
+                    item
+                    xs={12}
+                    sm={2}
+                    align="center"
+                    style={{ marginTop: "10px" }}
+                  >
+                    {/* Icons provided by https://icons8.com */}
+                    <a
+                      href="https://twitter.com/onecaseapp"
+                      className="social-media-icons"
+                    >
+                      <TwitterIcon />
+                    </a>
+                    <a
+                      href="https://www.instagram.com/onecaseapp/"
+                      className="social-media-icons"
+                    >
+                      <InstagramIcon />
+                    </a>
+                    <a className="social-media-icons">
+                      <YouTubeIcon />
+                    </a>
+                  </Grid>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
