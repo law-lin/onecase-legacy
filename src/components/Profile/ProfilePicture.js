4f0bf3c4ce324d9c1c72b6b09d7361c56e691f06
law-lin <lawrencelin101@gmail.com>
2020-07-30 02:16:05 -0400

    react-easy-crop, rotate and crop images

--- src/components/Profile/ProfilePicture.js
+++ src/components/Profile/ProfilePicture.js
@@ -1,13 +1,15 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, Fragment, useRef, useCallback } from "react";
 import { ButtonBase, Typography } from "@material-ui/core";
-
+import Slider from "@material-ui/core/Slider";
+import Cropper from "react-easy-crop";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
-
+import { getOrientation } from "get-orientation/browser";
+import { getCroppedImg, getRotatedImage } from "../canvasUtils";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
@@ -16,6 +18,8 @@ import DefaultProfilePic from "../../images/default-profile-pic.png";
 import { withFirebase } from "../Firebase";
 import ImageUploader from "react-images-upload";
 import Avatar from "react-avatar";
+import { IconButton } from "@material-ui/core";
+import CameraAltIcon from "@material-ui/icons/CameraAlt";
 
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -57,12 +61,59 @@ const useStyles = makeStyles({
     borderBottomLeftRadius: "50px",
     borderBottomRightRadius: "50px",
   },
+  crop: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: "80px",
+  },
+  controls: {
+    position: "absolute",
+    bottom: 0,
+    left: "50%",
+    width: "50%",
+    transform: "translateX(-50%)",
+    height: "80px",
+    display: "flex",
+    alignItems: "center",
+  },
+  sliderContainer: {
+    display: "flex",
+    flex: "1",
+    alignItems: "center",
+  },
+  sliderLabel: {
+    margin: "0 20px 0 20px",
+  },
+  slider: {
+    padding: "22px 0px",
+    marginLeft: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    margin: "0 16px",
+  },
+  dialogPaper: {
+    minHeight: "80vh",
+    maxHeight: "80vh",
+  },
 });
 
 function ProfilePicture(props) {
   const classes = useStyles();
 
+  const [newProfilePicture, setNewProfilePicture] = useState(null);
   const [open, setOpen] = useState(false);
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [zoom, setZoom] = useState(1);
+  const [imageSrc, setImageSrc] = useState(null);
+  const [rotation, setRotation] = useState(0);
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
+
+  const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+    setCroppedAreaPixels(croppedAreaPixels);
+  }, []);
+
   const { profilePicture } = props;
 
   const handleOpen = () => {
@@ -72,10 +123,50 @@ function ProfilePicture(props) {
   const handleClose = () => {
     setOpen(false);
   };
-  const handleChange = (e) => {
-    if (e.target.files[0]) {
-      const profilePicture = e.target.files[0];
-      props.firebase.uploadProfilePicture(profilePicture).on(
+
+  const onChange = (crop) => {
+    setCrop(crop);
+  };
+
+  function readFile(file) {
+    return new Promise((resolve) => {
+      const reader = new FileReader();
+      reader.addEventListener("load", () => resolve(reader.result), false);
+      reader.readAsDataURL(file);
+    });
+  }
+  const ORIENTATION_TO_ANGLE = {
+    "3": 180,
+    "6": 90,
+    "8": -90,
+  };
+  const handleChange = async (e) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0];
+      let imageDataUrl = await readFile(file);
+
+      // apply rotation if needed
+      const orientation = await getOrientation(file);
+      const rotation = ORIENTATION_TO_ANGLE[orientation];
+      if (rotation) {
+        imageDataUrl = await getRotatedImage(imageDataUrl, rotation);
+      }
+      setImageSrc(imageDataUrl);
+      setOpen(true);
+    }
+  };
+
+  const handleSave = useCallback(async () => {
+    try {
+      const croppedImage = await getCroppedImg(
+        imageSrc,
+        croppedAreaPixels,
+        rotation
+      );
+      console.log("done!", { croppedImage });
+      setNewProfilePicture(croppedImage);
+
+      props.firebase.uploadProfilePicture(croppedImage.croppedImage).on(
         "state_changed",
         (snapshot) => {
           // progress function ...
@@ -89,12 +180,16 @@ function ProfilePicture(props) {
         },
         () => {
           // complete function ...
-          props.firebase.uploadProfilePictureURL(profilePicture);
+          props.firebase.uploadProfilePictureURL(croppedImage);
+          setOpen(false);
         }
       );
+    } catch (e) {
+      console.error(e);
     }
-  };
+  }, [imageSrc, croppedAreaPixels, rotation]);
 
+  const fileUpload = useRef(null);
   return (
     <React.Fragment>
       {props.editable && (
@@ -112,7 +207,17 @@ function ProfilePicture(props) {
             style={{ position: "relative" }}
             src={profilePicture}
           ></Avatar>
-          <Button className={classes.change} onClick={handleOpen}>
+          <input
+            type="file"
+            ref={fileUpload}
+            style={{ display: "none" }}
+            onChange={handleChange}
+          />
+          <Button
+            style={{ display: "none" }}
+            className={classes.change}
+            onClick={() => fileUpload.current.click()}
+          >
             change profile picture
           </Button>
           <Dialog
@@ -120,17 +225,70 @@ function ProfilePicture(props) {
             onClose={handleClose}
             fullWidth={true}
             maxWidth={"lg"}
+            height
+            classes={{ paper: classes.dialogPaper }}
             PaperProps={{
               style: { backgroundColor: "#E4E4E4" },
             }}
           >
-            <DialogTitle></DialogTitle>
-            <DialogContent dividers></DialogContent>
+            <DialogContent>
+              <div className={classes.crop}>
+                <Cropper
+                  image={imageSrc}
+                  crop={crop}
+                  rotation={rotation}
+                  zoom={zoom}
+                  aspect={3 / 3}
+                  onCropChange={setCrop}
+                  onRotationChange={setRotation}
+                  onCropComplete={onCropComplete}
+                  onZoomChange={setZoom}
+                />
+              </div>
+              <div className={classes.controls}>
+                <div className={classes.sliderContainer}>
+                  <Typography
+                    variant="overline"
+                    classes={{ root: classes.sliderLabel }}
+                  >
+                    Zoom
+                  </Typography>
+                  <Slider
+                    value={zoom}
+                    min={1}
+                    max={3}
+                    step={0.1}
+                    aria-labelledby="Zoom"
+                    onChange={(e, zoom) => setZoom(zoom)}
+                    classes={{ container: classes.slider }}
+                  />
+                </div>
+                <div className={classes.sliderContainer}>
+                  <Typography
+                    variant="overline"
+                    classes={{ root: classes.sliderLabel }}
+                  >
+                    Rotation
+                  </Typography>
+                  <Slider
+                    value={rotation}
+                    min={0}
+                    max={360}
+                    step={1}
+                    aria-labelledby="Rotation"
+                    classes={{ container: classes.slider }}
+                    onChange={(e, rotation) => setRotation(rotation)}
+                  />
+                </div>
+              </div>
+            </DialogContent>
             <DialogActions>
               <Button onClick={handleClose} color="primary">
                 Cancel
               </Button>
-              <Button color="primary">Save</Button>
+              <Button onClick={handleSave} color="primary">
+                Save
+              </Button>
             </DialogActions>
           </Dialog>
         </div>
